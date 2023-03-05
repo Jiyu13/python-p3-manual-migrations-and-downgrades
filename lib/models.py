fabbc2018c33a8888803883addafc74c1d299a13
Ziru Zou <jiyucoding@yahoo.com>
2023-03-04 22:37:54 -0500

    rename birthday column to born

--- lib/models.py
+++ lib/models.py
@@ -17,13 +17,19 @@ engine = create_engine('sqlite:///migrations_test.db')
 Base = declarative_base()
 
 class Student(Base):
+    # __tablename__ = 'students'
+    # 1. change table name
     __tablename__ = 'students'
 
     id = Column(Integer(), primary_key=True)
     name = Column(String(), index=True)
     email = Column(String(55))
     grade = Column(Integer())
-    birthday = Column(DateTime())
+    # birthday = Column(DateTime())
+
+    # # rename a column name
+    born = Column(DateTime())
+
     enrolled_date = Column(DateTime(), default=datetime.now())
 
     def __repr__(self):
